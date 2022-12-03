b1da43339e4dd907e1178b4c81af1b265e5ce1d5
Анечка-Бубочка <j.t.leroy@mail.ru>
2022-12-03 20:10:08 +0300

    Add Ex002 Hello user

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello1, World!");
+﻿Console.Write ("Введите ваше имя");
+string username = Console.ReadLine ();
+Console.Write ("Привет,");
+Console.Write (username);
